a2888e8448b3a3b66cfb8244336600493ad23ac4
Parag <paraggawde1@gmail.com>
2024-10-30 11:55:01 +0530

    Update codes according to best coding practices

--- collection/src/UsesOfCollectors.java
+++ collection/src/UsesOfCollectors.java
@@ -1,3 +1,5 @@
+package collection.src;
+
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
--- collection/src/UsesOfFunction.java
+++ collection/src/UsesOfFunction.java
@@ -1,3 +1,5 @@
+package collection.src;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
