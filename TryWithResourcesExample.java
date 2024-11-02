10ed3728e3b8f20eb7689876408e1f1078196ff8
Parag <paraggawde1@gmail.com>
2024-11-02 20:35:40 +0530

    Add code for try-with-resources (try-catch example)

---
+++ TryWithResourcesExample.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResourcesExample {
+    public static void main(String[] args) {
+        String filePath = "sample-text-1.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
